731ffca01bda409658f6a13a5ed67e5d1d0ea99a
leoyoshii <leo_yoshii@outlook.com>
2020-03-20 19:33:45 -0300

    Realizando aula 20200320 testes no gerenciador de comissao - Atualizando metodo calcularComissaoComValorMinimo e calcularComissaoComFaixa

--- src/aula20200320/testesGerais/GerenciadorDeComissao.java
+++ src/aula20200320/testesGerais/GerenciadorDeComissao.java
@@ -6,4 +6,29 @@ public class GerenciadorDeComissao {
 		return valorDaVenda * (percentualDeComissao/100.00);
 	}
 
+	public static double calcularComissaoComValorMinimo(double valorMinimo, double valorDaVenda, double percentualDeComissao) {
+		double comissao = 0;
+		comissao = valorDaVenda * (percentualDeComissao/100.00);
+		if (comissao > valorMinimo){
+			return comissao;
+		} else {
+			return valorMinimo;
+		}
+	}
+
+	public static double calcularComissaoComFaixa(double valorMinimo,double valorMaximo, double valorDaVenda, double percentualDeComissao) {
+		double comissao = 0;
+		comissao = valorDaVenda * (percentualDeComissao/100.00);
+		if (comissao > valorMinimo){
+			if(comissao < valorMaximo){
+				return comissao;
+			}
+			else{
+				return valorMaximo;
+			}
+		} else {
+			return valorMinimo;
+		}
+	}
+
 }
